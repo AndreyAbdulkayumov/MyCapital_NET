7aa273b3dffa5f9c3cf79d314e4def8752708bc0
AndreyAbdulkayumov <86914394+AndreyAbdulkayumov@users.noreply.github.com>
2025-08-19 23:05:27 +0300

    Переименован проект для MAUI.

---
+++ MyCapital.MAUI/MauiProgram.cs
@@ -0,0 +1,42 @@
+﻿using Core;
+using Core.RateSourse_RussianCentralBank;
+using Microsoft.Extensions.Logging;
+using MyCapital.MAUI.Services;
+using Services.Interfaces;
+using ViewModels;
+
+namespace MyCapital.MAUI
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            // Models
+            builder.Services.AddTransient<IRateSource, RussianCentralBank>();
+
+            // ViewModels
+            builder.Services.AddTransient<MainPage_VM>();
+
+            // Views
+            builder.Services.AddTransient<MainPage>();
+
+            // Services
+            builder.Services.AddTransient<INotifications, Notifications>();
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
--- MyCapital_Mobile_MAUI/Pages/Field.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace MyCapital_Mobile_MAUI.Pages;
-
-public partial class Field : ContentView
-{
-	public Field()
-	{
-		InitializeComponent();
-	}
-}
