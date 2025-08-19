0043b6325492d3564779adb53af4624c6329ab26
AndreyAbdulkayumov <86914394+AndreyAbdulkayumov@users.noreply.github.com>
2025-08-19 17:54:18 +0300

    Убрано подчеркивание для всех Entry.

--- MyCapital_Mobile_MAUI/App.xaml.cs
+++ MyCapital_Mobile_MAUI/App.xaml.cs
@@ -9,6 +9,18 @@
             // Регистрация маршрута для дальнейшего перехода на эту страницу
             Routing.RegisterRoute(nameof(ErrorConnectionPage), typeof(ErrorConnectionPage));
 
+            // Убираем подчеркивание для всех Entry
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoUnderline", (handler, view) =>
+            {
+#if ANDROID
+            handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+#elif IOS || MACCATALYST
+            handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif WINDOWS
+                handler.PlatformView.BorderThickness = new Microsoft.UI.Xaml.Thickness(0);
+#endif
+            });
+
             MainPage = new AppShell();
         }
     }
